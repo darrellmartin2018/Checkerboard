428e50690b12e6154dc0f793dc9ea921c87c4b57
Darrell Martin <dwmkv9@mail.missouri.edu>
2018-02-01 18:45:28 -0600

    added getter functions

--- src/checkerboard/Checkerboard1.java
+++ src/checkerboard/Checkerboard1.java
@@ -5,6 +5,7 @@
  */
 package checkerboard;
 
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 
 /**
@@ -12,6 +13,7 @@ import javafx.scene.paint.Color;
  * @author darrellmartin
  */
 public class Checkerboard1 {
+    private AnchorPane anchorPane;
     private int numRows;
     private int numCols;
     private double boardWidth;
@@ -30,6 +32,40 @@ public class Checkerboard1 {
         this(numRows, numCols, boardWidth, boardHeight);
         this.lightColor = lightColor;
         this.darkColor = darkColor;
+    }
+    
+    public int getNumRows(){
+        return numRows;
+    }
+    
+    public int getNumCols(){
+        return numCols;
+    } 
+    
+    public double getWidth(){
+        return boardWidth;
+    }
+        
+    public double getHeight(){
+        return boardHeight;
+    }
+    
+    public Color getLightColor(){
+        return lightColor;
+    }
+    
+    public Color getDarkColor(){
+        return darkColor;
+    }
+    
+    private double getRectangleWidth(){
+        this.boardWidth = this.anchorPane.getWidth();
+        return Math.ceil(this.boardWidth / this.numCols);
+    }
+    
+    private double getRectangleHeight(){
+        this.boardHeight = this.anchorPane.getHeight();
+        return Math.ceil(this.boardHeight / this.numRows);
     }
     
 }
